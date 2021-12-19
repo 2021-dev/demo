332f25a71d5638ba866959490b0b1237ee70d6ad
隐藏的豆浆 <jianpenggong@outlook.com>
2021-12-19 19:24:35 +0800

    testdep

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName main
+ * @DEscription TODO
+ * @Author jpgong
+ * @Date 2021/12/19 19:04
+ **/
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
